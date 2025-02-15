3ab48419e8622aeddbab2c8bc60489bfd0d745be
krisbobo <kiyongbohbo@gmail.com>
2025-02-15 04:23:57 +0100

    Add function to enable a round of play

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function getComputerChoice() {
   const paper = 'Paper';
   const scissors = 'Scissors';
   if (decision === 0) {
-    return rock;
+    return rock.toLowerCase();
   }
   if (decision === 1) {
-    return paper;
+    return paper.toLowerCase();
   }
   if (decision === 2) {
-    return scissors;
+    return scissors.toLowerCase();
   }
 }
 
@@ -23,4 +23,32 @@ function getHumanChoice() {
   return choice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == 'rock' && computerChoice == 'scissors') {
+    console.log('You win! Rock beats scissors!');
+    humanScore++;
+  } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+    console.log('You win! Paper beats rock!');
+    humanScore++;
+  } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+    console.log('You win! Scissors beats paper!');
+    humanScore++;
+  } else if (humanChoice == 'scissors' && computerChoice == ' rock') {
+    console.log('You lose! Rock beats scissors!');
+    computerScore++;
+  } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+    console.log('You lose! Paper beats rock!');
+    computerScore++;
+  } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+    console.log('You lose! Scissors beats paper!');
+    computerScore++;
+  } else if (humanChoice === computerChoice) {
+    console.log("It's a tie! Play again!");
+  }
+  console.log(computerScore, humanScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
